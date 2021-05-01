a723223a5af51a53f3b2c060dba96afa03750af7
itaitoshiki <toshiki.i0918@gmail.com>
2021-05-01 17:40:11 +0900

    8-3追加

---
+++ prob8-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int ans = 1;
+
+    printf("階乗を求めたい数値を入力してください．>>> ");
+    scanf("%d", &x);
+    printf("%d! = ", x);
+    while(x > 0){
+        ans = ans * x;
+        x--;
+    }
+    printf("%d\n", ans);
+}
