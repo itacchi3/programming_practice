caa02e70123aa584fc0680590a049c5e2cb1be98
itaitoshiki <toshiki.i0918@gmail.com>
2021-05-03 18:59:31 +0900

    prob8-16追加

--- prob8-16.c
+++ prob8-16.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 int main() {
-    int i;
+    int i, j, max_index;
     int array[9];
-    int max;
+    int max, max_temp;
 
     srand((unsigned int)time(NULL));
 
@@ -13,13 +13,28 @@ int main() {
         array[i] = rand() % 100 + 1;
     }
 
-    max = array[0];  //最大値を配列の先頭要素で初期化
-
-    //最大値のサーチ
-    for (i = 0; i < 10; i++) {
-        if (max < array[i]) {  //配列i番目の数値がmaxよりも大きかったら
-            max = array[i];  // maxに配列i番目の数値を格納
-        }
+    printf("ソート前\n");
+    for (i = 0; i < 10; ++i) {
+        printf("%d\n", array[i]);
     }
-    printf("最大値：%d\n", max);
+
+    for (i = 0; i < 10; i++) {
+        //最大値を先頭要素で初期化
+        max = array[i];
+        max_index = i;
+        //最大値のサーチ
+        for (j = i; j < 10; j++) {
+            if (max < array[j]) {  //配列i番目の数値がmaxよりも大きかったら
+                max = array[j];  // maxに配列i番目の数値を格納
+                max_index = j;
+            }
+        }
+        //配列の最初の数と入れ替える
+        array[max_index] = array[i];
+        array[i] = max;
+    }
+    printf("ソート後\n");
+    for (i = 0; i < 10; ++i) {
+        printf("%d\n", array[i]);
+    }
 }
