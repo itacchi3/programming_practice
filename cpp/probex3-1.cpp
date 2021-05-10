b93fd47d996dd58317af8c82ba745ba1351dd568
itaitoshiki <toshiki.i0918@gmail.com>
2021-05-11 06:52:02 +0900

    probex3-1追加(文字列の長さおかしい)

--- cpp/probex3-1.cpp
+++ cpp/probex3-1.cpp
@@ -1,39 +1,20 @@
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-//  整数値を比較し、大きいほうを返す関数
-int max(int, int);
-//  実数を比較し、大きいほうを返す関数
-double max(double, double);
-//  文字列を比較し、長いほうを返す関数
-string max(string, string);
+template <typename T>
+T max_T(T a, T b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
 
 int main() {
-    cout << max(1, 2) << endl;
-    cout << max(1.75, 3.12) << endl;
+    cout << max_T<int>(1, 2) << endl;
+    cout << max_T<double>(1.75, 3.12) << endl;
     string s1 = "aiu", s2 = "eo";
-    cout << max(s1, s2) << endl;
+    cout << max_T<string>(s1, s2) << endl;  //文字列の長さ比較できない
     return 0;
-}
-
-int max(int a, int b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
-}
-double max(double a, double b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
-}
-string max(string a, string b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
 }
